6bba07de32a167d0c22b8345290f18beb1094e76
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-08-20 10:56:25 -0500

    commit tree 51d131ee5daef9d86365eceec936b848e7a549b4

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(arrayOfNames, eventName){
+    let newArray = []
+    for(let i = 0; i < arrayOfNames.length; i++){
+        newArray[i] = `Thank you, ${arrayOfNames[i]}, for the wonderful ${eventName} gift!`
+    }
+    return newArray
+}
+writeCards(["Ada", "Brendan", "Ali"], "birthday")
+
+function countDown(number) {
+    while(number >= 0){
+        console.log(number)
+        number--
+    }
+}
